d1b0f2a73f9fac2d4f5fa1f614338f3adc31f5a9
rtpj26 <ronilo@akubo.net>
2016-08-31 15:51:53 +0800

    All sort working

--- classes/Product.class.php
+++ classes/Product.class.php
@@ -19,39 +19,24 @@ class Product{
 
 	public static function getTires($sortBy){
 		$sql = empty($sortBy) ? "SELECT * FROM TIRE A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 1" :  "SELECT * FROM TIRE A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 1 ORDER BY " . $sortBy;
-		if(empty($sortBy)){
-			$pdo_1 = self::$pdo->prepare($sql);
-			//$pdo_1 = self::$pdo->prepare("SELECT * FROM TIRE A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 1");
-			$pdo_1->execute();
-		}else{
-			$pdo_1 = self::$pdo->prepare($sql);
-			//$pdo_1 = self::$pdo->prepare("SELECT * FROM TIRE A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 1 ORDER BY ':sortby'");
-			$pdo_1->execute(array(":sortby" => $sortBy));
-		}
+		$pdo_1 = self::$pdo->prepare($sql);
+		$pdo_1->execute();
 		$result = $pdo_1->fetchAll(PDO::FETCH_ASSOC);
 		return $result;
 	}
 
 	public static function getWheels($sortBy){
-		if($sortBy == ""){
-			$pdo_1 = self::$pdo->prepare("SELECT * FROM WHEEL A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 2");
-			$pdo_1->execute();
-		}else{
-			$pdo_1 = self::$pdo->prepare("SELECT * FROM WHEEL A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 2 ORDER BY :sortby");
-			$pdo_1->execute(array(":sortby" => $sortBy));
-		}
+		$sql = empty($sortBy) ? "SELECT * FROM WHEEL A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 2" :  "SELECT * FROM WHEEL A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 2 ORDER BY " . $sortBy;
+		$pdo_1 = self::$pdo->prepare($sql);
+		$pdo_1->execute();
 		$result = $pdo_1->fetchAll(PDO::FETCH_ASSOC);
 		return $result;
 	}
 
 	public static function getBatteries($sortBy){
-		if($sortBy == ""){
-			$pdo_1 = self::$pdo->prepare("SELECT * FROM BATTERY A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 3");
-			$pdo_1->execute();
-		}else{
-			$pdo_1 = self::$pdo->prepare("SELECT * FROM BATTERY A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 3 ORDER BY :sortby");
-			$pdo_1->execute(array(":sortby" => $sortBy));
-		}
+		$sql = empty($sortBy) ? "SELECT * FROM BATTERY A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 3" :  "SELECT * FROM BATTERY A INNER JOIN PRODUCT B WHERE B.PRODUCT_ID = A.PRODUCT_ID AND B.PRODUCT_TYPE = 3 ORDER BY " . $sortBy;
+		$pdo_1 = self::$pdo->prepare($sql);
+		$pdo_1->execute();
 		$result = $pdo_1->fetchAll(PDO::FETCH_ASSOC);
 		return $result;
 	}
